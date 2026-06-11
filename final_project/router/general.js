d6a676e9ef2b208d389df3a586e9f34e99cd79a2
aditya2006pec-hash <aditya.2006.pec@gmail.com>
2026-06-10 23:50:55 -0400

    add task-1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let DisplayBooks = JSON.stringify(books);
+  return res.status(202).send(DisplayBooks);
 });
 
 // Get book details based on ISBN
